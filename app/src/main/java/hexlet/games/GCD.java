ce1ebdc926912ada74a1fadeaad09ac7f9790250
Rasskazova Ksenya <rasskazovaksenya@Rasskazovas-MacBook-Pro.local>
2024-11-01 10:56:42 +0300

    seven step

---
+++ app/src/main/java/hexlet/games/GCD.java
@@ -0,0 +1,34 @@
+package hexlet.games;
+import hexlet.code.Engine;
+
+public class GCD {
+    public static String gcd() {
+
+        Engine.greet();
+
+        int gcd = 0;
+        int x = 0;
+        int y = 0;
+
+        System.out.println("Find the greatest common divisor of given numbers.");
+
+        while (Engine.count < 3) {
+           x = Engine.getRandomNumber();
+           y = Engine.getRandomNumber();
+           for (int i = 1; i <= Math.min(x,y); i++) {
+               if (x % i == 0 && y % i == 0) {
+                   gcd = i;
+                   }
+           }
+            Engine.quest_answ(x + " " + y);
+            Engine.Equal(""+gcd);
+            Engine.count++;
+        }
+        if (Engine.count ==3) {
+            return "Congratulations, " + Engine.userName + "!";
+        }
+        else {
+            return Engine.Equal(""+gcd);
+        }
+    }
+}
---
+++ app/src/main/java/hexlet/games/Progression.java
@@ -0,0 +1,18 @@
+package hexlet.games;
+
+import hexlet.code.Engine;
+
+public class Progression {
+    public static String prog(){
+        int x = 0;
+        int num = 0;
+
+        Engine.greet();
+
+        System.out.println("What number is missing in the progression?");
+
+
+
+        return "";
+    }
+}

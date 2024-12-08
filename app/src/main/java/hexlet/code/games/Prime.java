37ce431c2d74f516ef35d669b942db82cd1e995a
Rasskazova Ksenya <rasskazovaksenya@Rasskazovas-MacBook-Pro.local>
2024-12-08 12:57:38 +0300

    fix Prime class

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,38 +1,45 @@
 package hexlet.code.games;
+
 import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 import static hexlet.code.Engine.NUMBER_OF_QUESTIONS;
 
 public class Prime {
+
     public static void prime() {
-        final int maxValue = 50;
-        final int minValue = 1;
         String[] questions = new String[NUMBER_OF_QUESTIONS];
         String[] answers = new String[NUMBER_OF_QUESTIONS];
 
+        generateQuestionsAndAnswers(questions, answers);
+
+        String instruction = "Answer 'yes' if the given number is prime. Otherwise answer 'no'.";
+        System.out.println(Engine.game(instruction, questions, answers));
+    }
+
+    private static void generateQuestionsAndAnswers(String[] questions, String[] answers) {
         for (int i = 0; i < NUMBER_OF_QUESTIONS; i++) {
-            int x = Utils.getRandomNumber(minValue, maxValue);
-            questions[i] = String.valueOf(x);
-            boolean flag = true;
-
-            if (x < 2) {
-                flag = false;
-            } else {
-                for (int j = 2; j <= Math.sqrt(x); j++) {
-                    if (x % j == 0) {
-                        flag = false;
-                        break;
-                    }
-                }
-            }
-
-            String correctAnswer = flag ? "yes" : "no";
-
-            answers[i] = correctAnswer;
+            int number = generateRandomNumber();
+            questions[i] = String.valueOf(number);
+            answers[i] = isPrime(number) ? "yes" : "no";
         }
-        String str = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-        System.out.println(Engine.game(str, questions, answers));
+    }
+
+    private static int generateRandomNumber() {
+        final int maxValue = 50;
+        final int minValue = 1;
+        return Utils.getRandomNumber(minValue, maxValue);
+    }
+
+    private static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int j = 2; j <= Math.sqrt(number); j++) {
+            if (number % j == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
-

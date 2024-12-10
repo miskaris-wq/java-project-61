af705f4bd7adf634ee4bc441d4c61ca48d881fe3
Rasskazova Ksenya <rasskazovaksenya@Rasskazovas-MacBook-Pro.local>
2024-12-10 21:36:24 +0300

    fix prime

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -8,27 +8,23 @@ import static hexlet.code.Engine.NUMBER_OF_QUESTIONS;
 public class Prime {
 
     public static void prime() {
+        final int maxValue = 50;
+        final int minValue = 1;
         String[] questions = new String[NUMBER_OF_QUESTIONS];
         String[] answers = new String[NUMBER_OF_QUESTIONS];
 
-        generateQuestionsAndAnswers(questions, answers);
+        generateQuestionsAndAnswers(questions, answers, minValue, maxValue);
 
-        String instruction = "Answer 'yes' if the given number is prime. Otherwise answer 'no'.";
+        String instruction = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
         System.out.println(Engine.game(instruction, questions, answers));
     }
 
-    private static void generateQuestionsAndAnswers(String[] questions, String[] answers) {
+    private static void generateQuestionsAndAnswers(String[] questions, String[] answers, int minValue, int maxValue) {
         for (int i = 0; i < NUMBER_OF_QUESTIONS; i++) {
-            int number = generateRandomNumber();
+            int number = Utils.getRandomNumber(minValue, maxValue);
             questions[i] = String.valueOf(number);
             answers[i] = isPrime(number) ? "yes" : "no";
         }
-    }
-
-    private static int generateRandomNumber() {
-        final int maxValue = 50;
-        final int minValue = 1;
-        return Utils.getRandomNumber(minValue, maxValue);
     }
 
     private static boolean isPrime(int number) {
